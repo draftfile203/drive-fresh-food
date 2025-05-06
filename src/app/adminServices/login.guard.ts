dca7135f8528f6f534ea0b848a0d71be7e947733
draftfile203 <luka.xelaia123@gmail.com>
2025-05-06 05:02:40 +0400

    Fix login guard logic and update dashboard redirect

--- src/app/adminServices/login.guard.ts
+++ src/app/adminServices/login.guard.ts
@@ -4,7 +4,7 @@ import { CanActivateFn, Router } from '@angular/router';
 export const loginGuard: CanActivateFn = (route, state) => {
   const router = inject(Router)
   if(typeof window !== 'undefined') {
-    const isLoggedIn = !!localStorage.getItem('isLoggedIn')
+    const isLoggedIn = localStorage.getItem('isLoggedIn')
 
     if(isLoggedIn) {
       return true
